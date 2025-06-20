9ac956547d2eff96aefad8c44ccab390bf92643b
santhosh007s <santhoshperumal745sp@gmail.com>
2025-06-21 00:03:23 +0530

    done and dusted with seo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,54 @@
+// import Hero from "@/components/sections/hero";
+// import NavBar from "@/components/ui/navbar-elem";
+// import TestimonialsSection from "@/components/sections/terstisec"
+// import {Component} from "@/components/sections/faq"
+// import Contact from "@/components/sections/contactus";
+// import { Cta } from "@/components/sections/Fcta";
+// import { PricingBasic } from "@/components/sections/services";
+// import { DeskPort } from "@/components/sections/portfoilo";
+// import { Footercomp } from "@/components/sections/footer";
+// import { WhyWebzy } from "@/components/sections/whywebzy";
+// import { MobilePort } from "@/components/sections/mob-port";
+
+// export default function Home() {
+//   return (
+   
+
+// <>
+//   <div className="flex flex-col">
+//     <NavBar />
+//     <Hero />
+//   </div>
+
+//   <div className="bg-black flex flex-col overflow-hidden">
+//     <WhyWebzy />
+//     <div id="works" >
+//         <div className=" hidden md:block ">
+//       <DeskPort />
+//     </div>
+//     <div className="block md:hidden">
+//       <MobilePort  />
+//     </div>
+
+//     </div>
+  
+//     <PricingBasic id="services" />
+//     <TestimonialsSection />
+//     <Contact id="contact" />
+//     <Cta />
+//     <Footercomp />
+//   </div>
+// </>
+
+
+//   );
+// }
+
+import Head from "next/head";
 import Hero from "@/components/sections/hero";
 import NavBar from "@/components/ui/navbar-elem";
-import TestimonialsSection from "@/components/sections/terstisec"
-import {Component} from "@/components/sections/faq"
+import TestimonialsSection from "@/components/sections/terstisec";
+import { Component } from "@/components/sections/faq";
 import Contact from "@/components/sections/contactus";
 import { Cta } from "@/components/sections/Fcta";
 import { PricingBasic } from "@/components/sections/services";
@@ -12,35 +59,36 @@ import { MobilePort } from "@/components/sections/mob-port";
 
 export default function Home() {
   return (
-   
+    <>
 
-<>
-  <div className="flex flex-col">
-    <NavBar />
-    <Hero />
-  </div>
+      {/* ✅ PAGE SECTIONS */}
+      <div className="flex flex-col">
+        <NavBar />
+        <Hero />
+      </div>
 
-  {/* Main Sections with Black Background */}
-  <div className="bg-black flex flex-col overflow-hidden">
-    <WhyWebzy />
-    <div id="works" >
-        <div className=" hidden md:block ">
-      <DeskPort />
-    </div>
-    <div className="block md:hidden">
-      <MobilePort  />
-    </div>
+      <div className="bg-black flex flex-col overflow-hidden">
+        <WhyWebzy />
+        <div id="works">
+          <div className="hidden md:block">
+            <DeskPort />
+          </div>
+          <div className="block md:hidden">
+            <MobilePort />
+          </div>
+        </div>
+<div id="services">
+        <PricingBasic  />
+</div>
+        <TestimonialsSection />
+        <div id="contact">
+        <Contact  />
 
-    </div>
-  
-    <PricingBasic id="services" />
-    <TestimonialsSection />
-    <Contact id="contact" />
-    <Cta />
-    <Footercomp />
-  </div>
-</>
-
-
+        </div>
+        <Cta />
+        <Footercomp />
+      </div>
+    </>
   );
 }
+
